50abe1c7c2e7fbe8b4563a97f0086d8d541844d8
Arkadiy <arkada38@outlook.com>
2016-08-17 16:47:09 +0700

    Until Task 5 2

---
+++ Task_05.h
@@ -0,0 +1,21 @@
+//
+// Created by MAN on 8/17/2016.
+//
+
+#ifndef CPPLESSONS_TASK_05_H
+#define CPPLESSONS_TASK_05_H
+void task_05_1();
+void task_05_2();
+void task_05_3();
+void task_05_4();
+void task_05_5();
+void task_05_6();
+void task_05_7();
+void task_05_8();
+void task_05_9();
+void task_05_10();
+void task_05_11();
+void task_05_12();
+void task_05_13();
+void task_05_14();
+#endif //CPPLESSONS_TASK_05_H
--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 
 // #include "Task_03.h"
-#include "Task_04.h"
+// #include "Task_04.h"
+#include "Task_05.h"
 
 int power(int x, unsigned p);
 
 int main() {
     cout << "Hello, World!" << endl;
 
-    task_04_14();
+    task_05_2();
 
     return 0;
 }
