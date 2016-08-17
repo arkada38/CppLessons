c6dcaf0244966f62257a5ab861836cd9f06ae658
Arkadiy <arkada38@outlook.com>
2016-08-17 14:33:39 +0700

    Task 4

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-#include "Task_03.h"
+// #include "Task_03.h"
 #include "Task_04.h"
 
 int power(int x, unsigned p);
@@ -10,7 +10,7 @@ int power(int x, unsigned p);
 int main() {
     cout << "Hello, World!" << endl;
 
-    task_04_6();
+    task_04_14();
 
     return 0;
 }
