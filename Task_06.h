779271a558d1f56cca711521b5797f8848a5ea5d
Arkadiy <arkada38@outlook.com>
2016-08-21 09:14:02 +0700

    Until Task 6 7

---
+++ Task_06.h
@@ -0,0 +1,19 @@
+//
+// Created by MAN on 8/18/2016.
+//
+
+#ifndef CPPLESSONS_TASK_06_H
+#define CPPLESSONS_TASK_06_H
+void task_06_1();
+void task_06_2();
+void task_06_3();
+void task_06_4();
+void task_06_5();
+void task_06_6();
+void task_06_7();
+void task_06_8();
+void task_06_9();
+void task_06_10();
+void task_06_11();
+void task_06_12();
+#endif //CPPLESSONS_TASK_06_H
--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 // #include "Task_03.h"
 // #include "Task_04.h"
-#include "Task_05.h"
+// #include "Task_05.h"
+#include "Task_06.h"
 
 int main() {
     cout << "Hello, World!" << endl;
 
-    task_05_14();
+    task_06_7();
 
     return 0;
 }
